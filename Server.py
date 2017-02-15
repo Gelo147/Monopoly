218a77cfc07528bb521875af02001dd182f93348
Gelo147 <gelo147@gmail.com>
2017-02-15 21:06:26 +0000

    Changes already made... Mistakes, reformat... well a complete makeover

--- Server.py
+++ Server.py
@@ -1,105 +1,110 @@
 from socket import *
 from json import dumps, loads
 from threading import Thread
-#
-# Discovery on port 44445
-# All other messages on port 6969
-#
+from random import randint
 
 
 class Server:
-    def __init__(self):
-        """/*
-        Port on which the the server is running
-        */"""
-        self.port = 6969
-        self.game_id = 0
-        self.started_games = {}  # ongoing games, {game_id: [players]}
-        #          where player a dictionary of {playerName : address}
-        self.open_games = {}  # games that can be joined, {game_id: [players]}
-        #                  where player a dictionary of {playerName : address}
-        self.boards_data = {}  # {game_id: data}
-        self.sock = socket(AF_INET, SOCK_DGRAM)
-        self.sock.bind(("", self.port))
-        self.started = False
-        self.discover = Thread(target=self.enable_discovery)
-        self.discover.start()
+    BROADCAST_PORT = 44470
+    SERVICE_PORT = 44469
 
-    def enable_discovery(self):
-        """Allow clients to discover the server"""
-        broadcastSock = socket(AF_INET, SOCK_DGRAM)
-        broadcastSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        broadcastSock.bind(('', 44445))
-        broadcastSock.settimeout(1)
-        print('Starting up broadcast service')
-        while True:
-            try:
-                data, address = broadcastSock.recvfrom(1024)
-                data = loads(data.decode())
-                if data["action"] == "open_games_request":
-                    data = {
-                        'games': self.open_games
-                    }
-                elif data["action"] == "create_game_request":
-                    self.open_games[self.game_id] = {data["player_name"]: address}
-                    data = {
-                        'game_id': self.game_id
-                    }
-                    self.game_id += 1
-                elif data["action"] == "join":
-                    if (data["game_id"] in self.open_games) and (len(self.open_games[data["game_id"]]) < 6):
-                        print("Keys : " + str(self.open_games[data["game_id"]].keys()))
-                        if data["player_name"] not in self.open_games[data["game_id"]].keys():
-                            self.open_games[data["game_id"]][data["player_name"]] = address
-                        data = {
-                            "game_id": data["game_id"],
-                            "game_state": self.open_games[data["game_id"]]
+    def __init__(self, broadcast_port=Server.BROADCAST_PORT, service_port = Server.SERVICE_PORT):
+        # Make the broadcast a separate thread
+        self._openBroadcast(broadcast_port)
+        self._openService(service_port)
 
-                        }
-                else:
-                    data = {
-                        'error': 'invalid request'
-                    }
-                serverState = {
-                    'port': self.port,
-                    'data': data
-                }
-                broadcastSock.sendto(dumps(serverState).encode(), address)
-            except timeout:
-                pass
-            except Exception as e:
-                print("Something went wrong")
-                print(e)
+    """
+    <-------------------- CLIENT TO SERVER ------------------------------>
+        Following function get called buy requests from client or as
+        "missing a word" to an action performed as a result of a broadcast
+    <-------------------------------------------------------------------->
+    """
+    def create_game(self, values, address):
+        # called by request handler <function=_handle_broadcast> when
+        # incoming message has <var=command> = CREATE
+        # Returns: Success / Failure message
+        pass
 
-    def recieve_messages(self):
-        recieveSock = socket(AF_INET, SOCK_DGRAM)
-        recieveSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        recieveSock.bind('', self.port)
-        while True:
-            try:
-                data, address = recieveSock.revcfrom(1024)
-                if data:
-                    self.process_messages(data, address)
-            except timeout:
-                pass
+    def poll_games(self, values, address):
+        # called by request handler <function=_handle_broadcast> when
+        # incoming message has <var=command> = POLL
+        # Returns: Game (ie. {"command": "GAME", "values": {"game":game_details}}
+        pass
 
-    def process_messages(self, data, address):
-        data = loads(data.decode())
-        try:
-            if self.started_games[data["game_id"]][data["player_name"]] == address:
-                # request from a vaild player
-                """
-                Perform action based on the rest of the message
-                """
-                if data["action"] == "update":
-                    # update the state of the board and send it to all players
-                    pass
-                elif data["action"] == "poll":
-                    # send the current state of the game
-                    pass
+    def join_game(self, values, address):
+        # called by request handler <function=_handle_broadcast> when
+        # incoming message has <var=command> = JOIN
+        # Returns: Success / Failure message
+        pass
 
-        except Exception as e:
-            print(e)
+    """
+    <-------------------- CLIENT TO SERVER ------------------------------>
+        Following function get called buy requests from client or as
+        "missing a word" to an action performed as a result of a request
+    <-------------------------------------------------------------------->
+    """
 
-if __name__ == '__main__':
-    Server()
+    def roll(self, values, address):
+        # called by request handler <function=_handle_request> when
+        # incoming message has <var=command> = ROLL
+        # generates two random numbers in range(1,7)
+        # Returns: ROLL message
+        pass
+
+    def buy(self, values, address):
+        # called by request handler <function=_handle_request> when
+        # incoming message has <var=command> = BUY
+        # 'buys' the property that the player is currently on
+        # Returns: Success / Failure message
+        pass
+
+    def sell(self, values, address):
+        # called by request handler <function=_handle_request> when
+        # incoming message has <var=command> = SELL
+        # 'sells' the properties defined in <var=ids> inside <var=values>
+        # Returns: PAY message
+        pass
+
+    def chat(self, values, address):
+        # called by request handler <function=_handle_request> when
+        # incoming message has <var=command> = CHAT
+        # Sends the message <var=text> that is in <var=values> onto other users,
+        # attaching values like username onto the message
+
+        # Returns: does not return ?? <-- NOTE * Not sure yet. * NOTE -->
+        pass
+
+    def turn(self):
+        # send message TURN to all clients
+        # informing them of whose turn it is
+        # <-- NOTE * self invoked sometimes?? * NOTE -->
+        pass
+
+    def go_to(self):
+        # inform all players where one player is
+        # {values: {palyer: int player_id, tile: int tile } }
+        pass
+
+    def pay(self):
+        # transaction between player and player or bank and player if
+        # to or from are None
+        # {"command": "PAY",
+        #    "values": {
+        #        "from": int player_id or None,
+        #        "to": int player_id or None,
+        #        "amount": int amount
+        #     }
+        # }
+        pass
+
+    def card(self):
+        # Comunity Chest / Chance cards
+        #
+        # {
+        #    "command": "CARD",
+        #    "values": {
+        #        "text": str text,
+        #        "is_bail": bool is_bail
+        #    }
+        # }
+        pass
