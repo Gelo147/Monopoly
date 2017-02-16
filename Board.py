90ea4bc90bb6afbbcc7a12e4a4a93892d6eb3c2c
conortwo <conor.t.email@gmail.com>
2017-02-16 15:05:40 +0000

    Early protoype for board
    Board should contain layout of all spaces and other relevant state information. Each client will hold own copy while server will hold seperate one. Updated in parallel when relevan actions occur

---
+++ Board.py
@@ -0,0 +1,9 @@
+class Board:
+    """The representation of the game board where most of the action takes place"""
+    def __init__(self):
+        self.spaces = [Space("Pass Go collect 200$","GO"),PropertySpace("Old Kent road",200]
+        
+
+    def __str__(self):
+        #text representation of board
+        return "[Board looks like this]"
