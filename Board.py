e796755cf6480cd862500de8f5b56d7c52e25e06
conortwo <conor.t.email@gmail.com>
2017-02-16 20:37:36 +0000

    Added board creation from file reading

--- Board.py
+++ Board.py
@@ -4,19 +4,19 @@ class Board:
 		self.vaildtypes = ["GO","PROPERTY","JAIL","FREE","GOTOJAIL]"]
         self.spaces = self.makeSpaces(filename)
         
-
     def __str__(self):
         #text representation of board
         return "[Board looks like this]"
-	def makeSpaces(self,filename):
-		try:
-			filehandle = open(filename,'r')
-			for line in filehandle:
-				args = line.split(*)
-				if args[0] == "PROPERTY":
-					self.spaces += [PropertySpace(args[0],args[2],args[3])]
-				else if args[0] in self.vaildtypes:
-					self.spaces += [Space(args[0],args[1])]
-		except FileNotFoundError:
-			print("Sorry couldn't find file '%s', please make sure it exists" % (filename))
-					
+    
+    def makeSpaces(self,filename):
+        try:
+            filehandle = open(filename,'r')
+            for line in filehandle:
+                    args = line.split("*")
+                    if args[0] == "PROPERTY":
+                            self.spaces += [PropertySpace(args[0],args[2],args[3])]
+                    else if args[0] in self.vaildtypes:
+                            self.spaces += [Space(args[0],args[1])]
+        except FileNotFoundError:
+                print("Sorry couldn't find file '%s', please make sure it exists" % (filename))
+                                
