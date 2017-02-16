083a14cf0c18726711a10a6c7494dbec498ec9aa
conortwo <conor.t.email@gmail.com>
2017-02-16 20:34:30 +0000

    Added board creation from file reading
    Can now read a file in specified format and make board dynamically from
    it

--- Board.py
+++ Board.py
@@ -1,9 +1,22 @@
 class Board:
     """The representation of the game board where most of the action takes place"""
-    def __init__(self):
-        self.spaces = [Space("Pass Go collect 200$","GO"),PropertySpace("Old Kent road",200]
+    def __init__(self,filename):
+		self.vaildtypes = ["GO","PROPERTY","JAIL","FREE","GOTOJAIL]"]
+        self.spaces = self.makeSpaces(filename)
         
 
     def __str__(self):
         #text representation of board
         return "[Board looks like this]"
+	def makeSpaces(self,filename):
+		try:
+			filehandle = open(filename,'r')
+			for line in filehandle:
+				args = line.split(*)
+				if args[0] == "PROPERTY":
+					self.spaces += [PropertySpace(args[0],args[2],args[3])]
+				else if args[0] in self.vaildtypes:
+					self.spaces += [Space(args[0],args[1])]
+		except FileNotFoundError:
+			print("Sorry couldn't find file '%s', please make sure it exists" % (filename))
+					
--- Space.py
+++ Space.py
@@ -1,6 +1,6 @@
 def Space:
     """A space is a single spot on the board with unique text"""
-    def __init__(self,text,space_type):
+    def __init__(self,space_type,text):
         self.text = text
         self.type = space_type
 
