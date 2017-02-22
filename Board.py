a609370d2fb8f997c832c595a0800723f7a1aba8
conortwo <conor.t.email@gmail.com>
2017-02-22 19:35:52 +0000

    Update Board.py

--- Board.py
+++ Board.py
@@ -8,6 +8,7 @@ import random
 class Board:
     """The representation of the game board where most of the action takes place"""
     def __init__(self,filename,playernames):
+        #takes in a text file describing every space and a dictionary of player names to ids
         self._vaildtypes = ["GO","PROPERTY","JAIL","FREE","GOTOJAIL","DECK","TAX"]
         self._vaildcardtypes = ["COLLECT","BAIL","PAY","GOTO"]
         self._property_sets = {}
@@ -26,6 +27,7 @@ class Board:
             
     
     def _makeSpaces(self,filename):
+        #parse a textfile to create appropiate space objects
         spaces = []
         decks = {}
         try:
@@ -35,11 +37,13 @@ class Board:
                     if args[0] == "PROPERTY":
                         spaces += [PropertySpace(args[1],int(args[2]),args[3])]
                         if args[3] not in self._property_sets:
+                            #property sets mantains name of each set and the max number of properties in it
                             self._property_sets[args[3]] = [[],1]
                         else:
                             self._property_sets[args[3]][1]+=1
                     elif args[0] == "DECK":
                         if args[1] not in decks:
+                            #only make each deck once and share across each space of same name
                             deck = self._makeDeck(args[2])
                             deckSpace = DeckSpace(args[1],deck)
                             decks[args[1]] = deckSpace
@@ -74,14 +78,18 @@ class Board:
                 exit()
                              
     def getSize(self):
+        #returns the number of valid positions on the board e.g 39 for the standard 40sq
         return len(self._spaces) -1
                              
     def getSpace(self,pos):
+        #returns the space object at a specified position pos
         return self._spaces[pos]
                              
     def getPlayer(self,player_id):
+        #returns the player object with given id
         return self._players[player_id]
 
     def getPlayerList(self):
+        #returns a copy of the list of players in game
         player_list = list(self._players)
         return player_list
