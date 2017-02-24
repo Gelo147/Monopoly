97be44a9d8cff697ad81ee6578b1a6a1358fc47f
MurkyMartin <martinjkelly5@gmail.com>
2017-02-24 15:03:25 +0000

    More efficient and added button functionality
    Buttons open a new window when clicked. Box to the right works

--- workingGUI.py
+++ workingGUI.py
@@ -1,139 +1,107 @@
 from tkinter import *
+from tkinter import scrolledtext as st
+from tkinter import ttk
 
-class Wid():
-	def __init__(self, parent):
-		#Parent and main container
-		self.myParent = parent
-		self.main_container = Frame(parent, background="gray")
-		self.main_container.pack(side="top", fill="both", expand=True)
+class Wid(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+        self.marker = False
+        #Parent and main container
+        self.myParent = parent
+        self.main_container = Frame(parent, background="gray")
+        self.main_container.pack(side="top", fill="both", expand=True)
 
-		#Top frame for top horizontal bar
-		self.top_frame = Frame(self.main_container, background="green")
-		self.top_frame.pack(side="top", fill="both", expand=False)
+        #Top frame for top horizontal bar
+        self.top_frame = Frame(self.main_container, background="green")
+        self.top_frame.pack(side="top", fill="both", expand=False)
 
+        #Bottom frame for everything else
+        self.bottom_frame = Frame(self.main_container, background="yellow")
+        self.bottom_frame.pack(side="bottom", fill="both", expand=True)
 
-		self.bottom_frame = Frame(self.main_container, background="yellow")
-		self.bottom_frame.pack(side="bottom", fill="both", expand=True)
+        #Buffer labels due to grid being a prick
+        #It's kinda cheating but it works
+        self.buffer1 = Label(self.top_frame, height=2, width=5, bg="blue")
+        self.buffer1.grid(row=0, column=1)
 
-		#Adding test label and quit button
-		self.label1 = Label(self.top_frame, text="mgh", height=2, width=5, fg="red")
-		self.top_frame.grid_rowconfigure(0, weight=1)
-		self.top_frame.grid_columnconfigure(5, weight=1)
-		self.label1.grid(row=0, column=5)
+        self.buffer2 = Label(self.top_frame, height=2, width=5, bg="blue")
+        self.buffer2.grid(row=0, column=2)
 
-		#Quit button needed as gui diables normal toolbars and forces constant fullscreen
-		self.quuit = Button(self.top_frame, text="X", command=root.destroy, height=2, width=5, fg="green")
-		self.quuit.grid(row=0, column=3)
+        self.buffer3 = Label(self.top_frame, height=2, width=5, bg="blue")
+        self.buffer3.grid(row=0, column=3)
 
-		#Left frame containing board and maybe labels
-		self.left_frame = Frame(self.bottom_frame, background="purple")
-		self.left_frame.grid(row=0, column=0)
+        #Adding test label and quit button
+        self.money = Label(self.top_frame, text="$$$$", height=2, width=10, fg="red")
+        self.money.grid(row=0, column=4)
 
-		#Right frame for log and other bits
-		self.right_frame = Frame(self.bottom_frame, background="blue", height=80, width=50)
-		self.bottom_frame.grid_columnconfigure(11, weight=1)
-		self.right_frame.grid(row=0, column=11)
+        #Quit button needed as gui diables normal toolbars and forces constant fullscreen
+        self.quuit = Button(self.top_frame, text="X", command=root.destroy, height=2, width=5, fg="green")
+        self.quuit.grid(row=0, column=0)
 
-		self.chat_box = Text(self.right_frame, bg="grey", width=20, height=5)
-		self.chat_box.pack(side="bottom", fill="x", expand=False)
+        #Left frame containing board and maybe labels
+        self.left_frame = Frame(self.bottom_frame, background="purple")
+        self.left_frame.grid(row=0, column=0)
 
-		#Test label
-		self.log_label = Label(self.right_frame, bg="blue", width=50, height=62)
-		self.log_label.pack(side="bottom", fill="both", expand=True)
+        #Right frame for log and other bits
+        self.right_frame = Frame(self.bottom_frame, background="blue", height=80, width=50)
+        self.bottom_frame.grid_columnconfigure(11, weight=1)
+        self.right_frame.grid(row=0, column=11)
 
+        #Chat_box
+        self.chat_box = st.ScrolledText(self.right_frame, width=30, height=0, undo=True)
+        self.chat_box.pack(side="bottom", fill="x", expand=False)
+        self.chat_box.bind("<Return>", self.toLabel)
 
-		#Making the buttons
-		#I WILL MAKE THIS MORE EFFICIENT
-		self.b0 = Button(self.left_frame, text="b0", height=5, width=10)
-		self.b1 = Button(self.left_frame, text="b1", height=5, width=10)
-		self.b2 = Button(self.left_frame, text="b2", height=5, width=10)
-		self.b3 = Button(self.left_frame, text="b3", height=5, width=10)
-		self.b4 = Button(self.left_frame, text="b4", height=5, width=10)
-		self.b5 = Button(self.left_frame, text="b5", height=5, width=10)
-		self.b6 = Button(self.left_frame, text="b6", height=5, width=10)
-		self.b7 = Button(self.left_frame, text="b7", height=5, width=10)
-		self.b8 = Button(self.left_frame, text="b8", height=5, width=10)
-		self.b9 = Button(self.left_frame, text="b9", height=5, width=10)
-		self.b10 = Button(self.left_frame, text="b10", height=5, width=10)
-		self.b11 = Button(self.left_frame, text="b11", height=5, width=10)
-		self.b12 = Button(self.left_frame, text="b12", height=5, width=10)
-		self.b13 = Button(self.left_frame, text="b13", height=5, width=10)
-		self.b14 = Button(self.left_frame, text="b14", height=5, width=10)
-		self.b15 = Button(self.left_frame, text="b15", height=5, width=10)
-		self.b16 = Button(self.left_frame, text="b16", height=5, width=10)
-		self.b17 = Button(self.left_frame, text="b17", height=5, width=10)
-		self.b18 = Button(self.left_frame, text="b18", height=5, width=10)
-		self.b19 = Button(self.left_frame, text="b19", height=5, width=10)
-		self.b20 = Button(self.left_frame, text="b20", height=5, width=10)
-		self.b21 = Button(self.left_frame, text="b21", height=5, width=10)
-		self.b22 = Button(self.left_frame, text="b22", height=5, width=10)
-		self.b23 = Button(self.left_frame, text="b23", height=5, width=10)
-		self.b24 = Button(self.left_frame, text="b24", height=5, width=10)
-		self.b25 = Button(self.left_frame, text="b25", height=5, width=10)
-		self.b26 = Button(self.left_frame, text="b26", height=5, width=10)
-		self.b27 = Button(self.left_frame, text="b27", height=5, width=10)
-		self.b28 = Button(self.left_frame, text="b28", height=5, width=10)
-		self.b29 = Button(self.left_frame, text="b29", height=5, width=10)
-		self.b30 = Button(self.left_frame, text="b30", height=5, width=10)
-		self.b31 = Button(self.left_frame, text="b31", height=5, width=10)
-		self.b32 = Button(self.left_frame, text="b32", height=5, width=10)
-		self.b33 = Button(self.left_frame, text="b33", height=5, width=10)
-		self.b34 = Button(self.left_frame, text="b34", height=5, width=10)
-		self.b35 = Button(self.left_frame, text="b35", height=5, width=10)
-		self.b36 = Button(self.left_frame, text="b36", height=5, width=10)
-		self.b37 = Button(self.left_frame, text="b37", height=5, width=10)
-		self.b38 = Button(self.left_frame, text="b38", height=5, width=10)
-		self.b39 = Button(self.left_frame, text="b39", height=5, width=10)
+        #Log
+        self.log = st.ScrolledText(self.right_frame, width=90, wrap="word", height=50)
+        self.log.pack(side="left", fill="both", expand=True)
 
-		#Positioning buttons
-		#WILL BE MORE NICE
-		self.b0.grid(row=0, column=0)
-		self.b1.grid(row=0, column=1)
-		self.b2.grid(row=0, column=2)
-		self.b3.grid(row=0, column=3)
-		self.b4.grid(row=0, column=4)
-		self.b5.grid(row=0, column=5)
-		self.b6.grid(row=0, column=6)
-		self.b7.grid(row=0, column=7)
-		self.b8.grid(row=0, column=8)
-		self.b9.grid(row=0, column=9)
-		self.b10.grid(row=0, column=10)
-		self.b11.grid(row=1, column=10)
-		self.b12.grid(row=2, column=10)
-		self.b13.grid(row=3, column=10)
-		self.b14.grid(row=4, column=10)
-		self.b15.grid(row=5, column=10)
-		self.b16.grid(row=6, column=10)
-		self.b17.grid(row=7, column=10)
-		self.b18.grid(row=8, column=10)
-		self.b19.grid(row=9, column=10)
-		self.b20.grid(row=10, column=10)
-		self.b21.grid(row=10, column=9)
-		self.b22.grid(row=10, column=8)
-		self.b23.grid(row=10, column=7)
-		self.b24.grid(row=10, column=6)
-		self.b25.grid(row=10, column=5)
-		self.b26.grid(row=10, column=4)
-		self.b27.grid(row=10, column=3)
-		self.b28.grid(row=10, column=2)
-		self.b29.grid(row=10, column=1)
-		self.b30.grid(row=10, column=0)
-		self.b31.grid(row=9, column=0)
-		self.b32.grid(row=8, column=0)
-		self.b33.grid(row=7, column=0)
-		self.b34.grid(row=6, column=0)
-		self.b35.grid(row=5, column=0)
-		self.b36.grid(row=4, column=0)
-		self.b37.grid(row=3, column=0)
-		self.b38.grid(row=2, column=0)
-		self.b39.grid(row=1, column=0)
+        #Making buttons
+        bh = 4
+        bw = 10
+        buttons = []
+        for i in range(40):
+            buttons.append(Button(self.left_frame, text="b"+str(i), height=bh, width=bw, command=self.create_window))
+        
+        #Positioning buttons
+        r = 0
+        c = 0
+        for button in buttons:
+            button.grid(row=r, column=c)
+            if r == 0 and c < 10:
+                c += 1
+            elif c == 10 and r < 10:
+                r += 1
+            elif r == 10 and c > 0:
+                c -= 1
+            else:
+                r -= 1
 
+    def toLabel(self, *args):
+        self.log.config(state="normal")
+        if not self.marker:
+            text = ">" + self.chat_box.get("1.0", "end-1c")
+            self.marker = True
+        else:
+            box = self.chat_box.get("1.0", END)
+            box = box[1:]
+            text = ">" + box
+        self.log.insert("1.0", text)
+        self.log.config(state="disabled")
+        self.chat_box.delete("1.0", END)
+
+    def create_window(self):
+        toplevel = Toplevel(self)
+        toplevel.wm_title("Card __")
+        name = Label(toplevel, text="name goes here", fg="purple", height=5, width=8)
+        name.pack(side="top", fill="both", expand=True)
 #Main
-root=Tk()
-w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-#Overrides tooltips and forces gui to persist on screen
-root.overrideredirect(1)
-#Makes full screen
-root.geometry("%dx%d+0+0" % (w, h))
-wid = Wid(root)
-root.mainloop()
+if __name__ == "__main__":
+    root=Tk()
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    #Overrides tooltips and forces gui to persist on screen
+    root.overrideredirect(1)
+    #Makes full screen
+    root.geometry("%dx%d+0+0" % (w, h))
+    wid = Wid(root)
+    root.mainloop()
